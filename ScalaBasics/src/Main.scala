3532b8d1635b34bd00f215934d81bd0f95a2d514
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-13 23:59:14 +0530

    added initial commit

---
+++ ScalaBasics/src/Main.scala
@@ -0,0 +1,112 @@
+package org.cricbuzz.ScalaLearning
+
+object Main {
+  def main(args: Array[String]): Unit = {
+//    println("Hello world!")
+    // usage of val
+//    val name = "John";
+//    println(name)
+
+    // usage of var
+//    var x = 1+1
+//    x = 4
+//    println(x)
+
+    //usage of blocks
+//    println({
+//      var y = 22*2
+//      y + 1
+//    })
+
+    // usage of functions
+//    var a = (z: Int)=> z + 1
+//    println(a(2))
+
+    // usage of methods
+//    def b (name: String, age: Int): String = {
+//      val obj: String = "name is: " + name + " and age is: " + age
+//      obj
+//    }
+//    println(b("John", 27))
+
+    // usage of classes
+//    class Greeter(prefix: String, suffix: String) {
+//      def greet(name: String): Unit =
+//        println(prefix + name + suffix)
+//    }
+//
+//    val greeter = new Greeter("Hello, ", "!")
+//    greeter.greet("Scala developer")
+
+    // usage of case classes
+//    case class Point(x: Int, y: Int)
+//    val point = Point(1, 2)
+//    val anotherPoint = Point(1, 2)
+//    val nextPoint = Point(2, 2)
+//
+//    if (point == anotherPoint) {
+//      println(s"$point and $anotherPoint are the same.")
+//    } else {
+//      println(s"$point and $anotherPoint are different.")
+//    }
+//
+//    if (point == nextPoint) {
+//      println(s"$point and $nextPoint are the same.")
+//    } else {
+//      println(s"$point and $nextPoint are different.")
+//    }
+
+    // usage of objects
+//    object IdFactory {
+//      private var counter = 0
+//
+//      def create(): Int = {
+//        counter += 1
+//        counter
+//      }
+//    }
+//
+//    val newId: Int = IdFactory.create()
+//    println(newId)
+//    val newerId: Int = IdFactory.create()
+//    println(newerId)
+
+    // usage of traits
+//    trait Greeter {
+//      def greet(name: String): Unit
+//    }
+//
+//    class CustomizableGreeter(prefix: String, postfix: String) extends Greeter {
+//      override def greet(name: String): Unit = {
+//        println(prefix + name + postfix)
+//      }
+//    }
+//
+//    val customGreeter = new CustomizableGreeter("How are you, ", "?")
+//    customGreeter.greet("Scala developer")
+//
+//    trait Greeters {
+//      def greet(name: String): Unit =
+//        println("Hello, " + name + "!")
+//    }
+//
+//    class DefaultGreeter extends Greeters
+//
+//    val greeter = new DefaultGreeter()
+//    greeter.greet("Scala developer")
+
+    // usage of List
+    val list: List[Any] = List(
+      "a string",
+      732, // an integer
+      'c', // a character
+      true, // a boolean value
+      () => "an anonymous function returning a string"
+    )
+
+    list.foreach(element => println(element))
+
+
+
+  }
+}
