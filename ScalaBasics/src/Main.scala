a91c08be28682adbe13d2523d7f3a2037a8e2696
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-22 23:31:47 +0530

    refactor: Remove example code and comments from ScalaLearning.Main object for cleaner main method

--- ScalaBasics/src/Main.scala
+++ ScalaBasics/src/Main.scala
@@ -95,18 +95,5 @@ object Main {
 //    val greeter = new DefaultGreeter()
 //    greeter.greet("Scala developer")
 
-    // usage of List
-    val list: List[Any] = List(
-      "a string",
-      732, // an integer
-      'c', // a character
-      true, // a boolean value
-      () => "an anonymous function returning a string"
-    )
-
-    list.foreach(element => println(element))
-
-
-
   }
 }
